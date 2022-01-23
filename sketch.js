deb5512753f9362f561dea3b9260fe5c14d385c5
AafreenKhan040896 <77886222+AafreenKhan040896@users.noreply.github.com>
2022-01-23 07:15:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var trex, ground;
+var trex_animation, groundImage;
+
+function preload(){
+  //load sound, image and animations in a variable
+  groundImage = loadImage("ground2.png");
+
+  trex_animation = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  trex = createSprite(50,180,20,40);
+  trex.addAnimation("running", trex_animation);
+  trex.scale = 0.5;
+
+
+  ground = createSprite(300,190,600,20);
+  ground.velocityX = -10;
+  ground.x = ground.width/2;
+  ground.addImage(groundImage);
+}
+
+
+function draw(){
+  //set background color 
+  background("white");
+  
+  if(ground.x < 0){
+    ground.x = ground.width/2;
+  }
+   
+  drawSprites();
+}
